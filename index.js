d22f1bb474d37fa503ea42810a7b8602bad9c321
Rain Zhang <xuanyuzhang2012@gmail.com>
2016-09-21 03:35:41 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+
+{% timeline [Google Sheet Code] [height] [language] [font] %}
+
+*/
+
+hexo.extenf.tag.register('timeline', function (args) {
+    var googleSheetCode = args[0];
+
+    var timeline = '<iframe src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=' + googleSheetCode +
+                   '&font=' + argv[3] +
+                   '&lang=' + argv[2] +
+                   '&initial_zoom=2&height=' + argv[1] +
+                   '" width="100%" height="' + argv[1] +
+                   '" frameborder="0"></iframe>';
+
+    return '<div class="timeline-container">' + timeline + '</div>';
+});
