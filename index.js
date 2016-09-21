0da3b444db4b224d469601834fc1663cd2111d93
Rain Zhang <xuanyuzhang2012@gmail.com>
2016-09-21 03:44:19 -0500

    fix typo

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ hexo.extend.tag.register('timeline', function (args) {
     var googleSheetCode = args[0];
 
     var timeline = '<iframe src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=' + googleSheetCode +
-                   '&font=' + argv[3] +
-                   '&lang=' + argv[2] +
-                   '&initial_zoom=2&height=' + argv[1] +
-                   '" width="100%" height="' + argv[1] +
+                   '&font=' + args[3] +
+                   '&lang=' + args[2] +
+                   '&initial_zoom=2&height=' + args[1] +
+                   '" width="100%" height="' + args[1] +
                    '" frameborder="0"></iframe>';
 
     return '<div class="timeline-container">' + timeline + '</div>';
